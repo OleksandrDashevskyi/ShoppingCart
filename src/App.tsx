b0bb08ed4a94b660fddebe6f9c7fe393e45af697
oleksandrdashevskyi <oleksandr.dashevskyi@andorhealth.com>
2022-12-26 22:31:02 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../src/pages/Home"
+import Store from "../src/pages/Store"
+import About from "../src/pages/About";
+import { Container } from "react-bootstrap"
+import Navbar from "./components/Navbar";
+
+function App() {
+
+  return (
+      <>
+          <Navbar />
+          <Container className="mb-4">
+              <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/store" element={<Store />} />
+                  <Route path="/about" element={<About />} />
+              </Routes>
+          </Container>
+      </>
+  )
+}
+
+export default App
